b7ae11fcb73a1615a06f1977249cb46efef436f4
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2022-11-29 10:01:11 +0400

    feat: add project

---
+++ README.md
@@ -0,0 +1,4 @@
+# animated rotate sphere
+
+* DEPLOYMENT IN GITHUB PAGES:
+##### See the ready Deployed project on [Github Pages](https://hacking-nassa-with-html.github.io/animated_rotate_sphere/).
---
+++ assets/js/mainScript.js
@@ -0,0 +1,18 @@
+
+const generatedContent = document.getElementById('containerForGeneratedContent')
+
+generatedContent.innerHTML = mainPageContent()
+
+function mainPageContent() {
+    const renderMainPageContent =
+        `<section id="mainSection">
+            <a href="https://github.com/Hacking-NASSA-with-HTML/play_with_nodejs"
+                target="_blank"><img width="149" height="149"
+                src="./assets/img/fork_me_left_red.png"
+                style="position: fixed; top:0; left:0" alt="Fork me on GitHub">
+            </a>
+            <h1 class="titleH1">Code For Joy</h1>
+        </section>
+        `
+    return renderMainPageContent
+}
